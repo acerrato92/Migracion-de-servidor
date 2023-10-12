c6634251a238e10d9c5f9197a9f8b255f93ba24a
acerrato92 <132726954+acerrato92@users.noreply.github.com>
2023-10-12 17:53:05 -0600

    cambio

--- app.js
+++ app.js
@@ -3,7 +3,60 @@ const express = require("express");
 const app = express(); 
 const task = require("./task.json");
 const port = 3001;
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config(); // Cargar variables de entorno
 
+// Array de usuarios predefinidos (esto debería reemplazarse por una base de datos real)
+const users = [
+  { id: 1, username: 'usuario1', password: 'contraseña1' },
+  { id: 2, username: 'usuario2', password: 'contraseña2' },
+];
+
+app.use(express.json());
+
+// Ruta de autenticación para generar un token JWT
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Buscar el usuario en el array (debería verificar en una base de datos real)
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales incorrectas' });
+  }
+
+  // Generar un token JWT
+  const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
+
+  res.json({ token });
+});
+
+// Ruta protegida que requiere autenticación
+app.get('/ruta-protegida', (req, res) => {
+  // Verificar el token en el encabezado de autorización
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  try {
+    // Verificar y decodificar el token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ mensaje: 'Acceso concedido', usuario: decoded.username });
+  } catch (error) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+});
+
+// Resto de la configuración de tu servidor
+
+app.listen(3000, () => {
+  console.log('Servidor Express en ejecución en el puerto 3000');
+});
 const Task = {
   id: "",
   isCompleted: false,
